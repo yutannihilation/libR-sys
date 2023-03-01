55a71349ec8f7860981480cb2dd7343766952d94
yutannihilation <yutannihilation@users.noreply.github.com>
2023-03-01 17:48:18 +0000

    Update bindings [skip ci]

--- bindings/bindings-macos-x86_64-R4.3-devel.rs
+++ bindings/bindings-macos-x86_64-R4.3-devel.rs
@@ -162,9 +162,9 @@ pub const R_MAJOR: &[u8; 2usize] = b"4\0";
 pub const R_MINOR: &[u8; 4usize] = b"3.0\0";
 pub const R_STATUS: &[u8; 29usize] = b"Under development (unstable)\0";
 pub const R_YEAR: &[u8; 5usize] = b"2023\0";
-pub const R_MONTH: &[u8; 3usize] = b"02\0";
-pub const R_DAY: &[u8; 3usize] = b"27\0";
-pub const R_SVN_REVISION: u32 = 83911;
+pub const R_MONTH: &[u8; 3usize] = b"03\0";
+pub const R_DAY: &[u8; 3usize] = b"01\0";
+pub const R_SVN_REVISION: u32 = 83918;
 pub const R_GE_definitions: u32 = 13;
 pub const R_GE_deviceClip: u32 = 14;
 pub const R_GE_group: u32 = 15;
@@ -4286,6 +4286,13 @@ extern "C" {
     ) -> R_altrep_class_t;
 }
 extern "C" {
+    pub fn R_make_altlist_class(
+        cname: *const ::std::os::raw::c_char,
+        pname: *const ::std::os::raw::c_char,
+        info: *mut DllInfo,
+    ) -> R_altrep_class_t;
+}
+extern "C" {
     pub fn R_altrep_inherits(x: SEXP, arg1: R_altrep_class_t) -> Rboolean;
 }
 pub type R_altrep_UnserializeEX_method_t = ::std::option::Option<
@@ -4408,6 +4415,10 @@ pub type R_altstring_Is_sorted_method_t =
     ::std::option::Option<unsafe extern "C" fn(arg1: SEXP) -> ::std::os::raw::c_int>;
 pub type R_altstring_No_NA_method_t =
     ::std::option::Option<unsafe extern "C" fn(arg1: SEXP) -> ::std::os::raw::c_int>;
+pub type R_altlist_Elt_method_t =
+    ::std::option::Option<unsafe extern "C" fn(arg1: SEXP, arg2: R_xlen_t) -> SEXP>;
+pub type R_altlist_Set_elt_method_t =
+    ::std::option::Option<unsafe extern "C" fn(arg1: SEXP, arg2: R_xlen_t, arg3: SEXP)>;
 extern "C" {
     pub fn R_set_altrep_UnserializeEX_method(
         cls: R_altrep_class_t,
@@ -4560,6 +4571,12 @@ extern "C" {
 }
 extern "C" {
     pub fn R_set_altstring_No_NA_method(cls: R_altrep_class_t, fun: R_altstring_No_NA_method_t);
+}
+extern "C" {
+    pub fn R_set_altlist_Elt_method(cls: R_altrep_class_t, fun: R_altlist_Elt_method_t);
+}
+extern "C" {
+    pub fn R_set_altlist_Set_elt_method(cls: R_altrep_class_t, fun: R_altlist_Set_elt_method_t);
 }
 extern "C" {
     pub fn R_GE_getVersion() -> ::std::os::raw::c_int;
